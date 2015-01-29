99ab8a6fe04f6879cfec8260f01624a57b6939c7
DzikuVx <pspychalski@gmail.com>
2015-01-29 21:25:14 +0100

    code

---
+++ daily_beeper.ino
@@ -0,0 +1,98 @@
+/*
+  This setup is for ATTiny85 with 
+  https://code.google.com/p/arduino-tiny/
+*/
+
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+
+#define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
+#define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
+
+#define BEEPER_PIN 2
+#define LED_PIN 0
+
+void setup() {
+  pinMode(BEEPER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  
+  // Blink on setup
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(100);
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+ 
+  setup_watchdog(7);
+  
+  enterSleep();
+}
+
+void enterSleep(void) {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sleep_mode();                        // System sleeps here
+  sleep_disable(); 
+}
+
+int iCounter = 0;
+
+//****************************************************************
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(int ii) {
+
+  byte bb;
+  int ww;
+  if (ii > 9 ) ii=9;
+  bb=ii & 7;
+  if (ii > 7) bb|= (1<<5);
+  bb|= (1<<WDCE);
+  ww=bb;
+
+  MCUSR &= ~(1<<WDRF);
+  // start timed sequence
+  WDTCR |= (1<<WDCE) | (1<<WDE);
+  // set new watchdog timeout value
+  WDTCR = bb;
+  WDTCR |= _BV(WDIE);
+}
+//****************************************************************  
+// Watchdog Interrupt Service / is executed when  watchdog timed out
+ISR(WDT_vect) {
+  iCounter++;
+}
+
+void loop() {
+  
+  if (iCounter == 20) {
+    wdt_disable();
+    
+    digitalWrite(BEEPER_PIN, HIGH);
+    delay(100);
+    digitalWrite(BEEPER_PIN, LOW);
+    delay(200);
+    digitalWrite(BEEPER_PIN, HIGH);
+    delay(100);
+    digitalWrite(BEEPER_PIN, LOW);
+    
+    delay(500);
+    
+    digitalWrite(BEEPER_PIN, HIGH);
+    delay(100);
+    digitalWrite(BEEPER_PIN, LOW);
+    delay(200);
+    digitalWrite(BEEPER_PIN, HIGH);
+    delay(100);
+    digitalWrite(BEEPER_PIN, LOW);
+    
+  } else {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW); 
+  }
+  
+  enterSleep();
+}
